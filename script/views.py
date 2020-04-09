043ca64f1b20bcdea01191f2bedb737529654b81
Myra Fah <FahMyra@JohnDeere.com>
2020-04-08 20:28:41 -0500

    added django built-in authentication and required authentication on script views

--- script/views.py
+++ script/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.decorators import login_required
 from .models import Chapter, Verse
 from .forms import ChapterForm, VerseForm
 
 # Chapter Views
+@login_required
 def chapter_list(request):
     chapters = Chapter.objects.order_by('chapter_num') 
     return render(request, 'script/chapter_list.html', {'chapters': chapters})
 
+@login_required
 def chapter_detail(request, pk):
     chapter = Chapter.objects.get(id=pk)
     return render(request, 'script/chapter_detail.html', {'chapter': chapter})
 
+@login_required
 def chapter_create(request):
     if request.method == 'POST':
         form = ChapterForm(request.POST)
@@ -22,6 +25,7 @@ def chapter_create(request):
         form = ChapterForm()
     return render(request, 'script/chapter_form.html', {'form': form})
 
+@login_required
 def chapter_edit(request, pk):
     chapter = Chapter.objects.get(pk=pk)
     if request.method == "POST":
@@ -33,19 +37,23 @@ def chapter_edit(request, pk):
         form = ChapterForm(instance=chapter)
     return render(request, 'script/chapter_form.html', {'form': form})
 
+@login_required
 def chapter_delete(request,pk):
     Chapter.objects.get(id=pk).delete()
     return redirect('chapter_list')
 
 # Verse Views
+@login_required
 def verse_list(request):
-    verses = Verse.objects.order_by('verse_num') 
+    verses = Verse.objects.order_by('chapter','verse_num') 
     return render(request, 'script/verse_list.html', {'verses': verses})
 
+@login_required
 def verse_detail(request, pk):
     verse = Verse.objects.get(id=pk)
     return render(request, 'script/verse_detail.html', {'verse': verse})
 
+@login_required
 def verse_create(request):
     if request.method == 'POST':
         form = VerseForm(request.POST)
@@ -56,6 +64,7 @@ def verse_create(request):
         form = VerseForm()
     return render(request, 'script/verse_form.html', {'form': form})
 
+@login_required
 def verse_edit(request, pk):
     verse = Verse.objects.get(pk=pk)
     if request.method == "POST":
@@ -67,6 +76,7 @@ def verse_edit(request, pk):
         form = VerseForm(instance=verse)
     return render(request, 'script/verse_form.html', {'form': form})
 
+@login_required
 def verse_delete(request,pk):
     Verse.objects.get(id=pk).delete()
     return redirect('verse_list')
