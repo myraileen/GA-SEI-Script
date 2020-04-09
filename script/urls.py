4523c756c60b5ac651ecb1f8785a4f025d233dc7
Myra Fah <FahMyra@JohnDeere.com>
2020-04-09 12:47:00 -0500

    updated project and app urls for rest framework

--- script/urls.py
+++ script/urls.py
@@ -1,21 +1,15 @@
 # nostaldja/urls.py
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    path('', views.chapter_list, name='chapter_list'),
-    path('chapters/<int:pk>/', views.chapter_detail, name='chapter_detail'),
-    path('chapters/new', views.chapter_create, name='chapter_create'),
-    path('chapters/<int:pk>/edit', views.chapter_edit, name='chapter_edit'),
-    path('chapters/<int:pk>/delete', views.chapter_delete, name='chapter_delete'),
-    path('verses/', views.verse_list, name='verse_list'),
-    path('verses/<int:pk>/', views.verse_detail, name='verse_detail'),
-    path('verses/new', views.verse_create, name='verse_create'),
-    path('verses/<int:pk>/edit', views.verse_edit, name='verse_edit'),
-    path('verses/<int:pk>/delete', views.verse_delete, name='verse_delete'),
-    path('books/', views.book_list, name='book_list'),
-    path('books/<int:pk>/', views.book_detail, name='book_detail'),
-    path('books/new', views.book_create, name='book_create'),
-    path('books/<int:pk>/edit', views.book_edit, name='book_edit'),
-    path('books/<int:pk>/delete', views.book_delete, name='book_delete'),
+    path('', views.ChapterList, name='chapter_list'),
+    path('chapters/<int:pk>', views.ChapterDetail.as_view(), name='chapter_detail'),
+    
+    path('verses/', views.VerseList, name='verse_list'),
+    path('verses/<int:pk>', views.VerseDetail.as_view(), name='verse_detail'),
+    
+    path('books/', views.BookList, name='book_list'),
+    path('books/<int:pk>', views.BookDetail.as_view(), name='book_detail'),
 ]
--- script_django/urls.py
+++ script_django/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path('', include('script.urls'))
+    path('', include('script.urls')),path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
