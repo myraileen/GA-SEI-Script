8cff68e65e9a2320c44abe6c82d322b5643768ed
Myra Fah <FahMyra@JohnDeere.com>
2020-04-09 00:57:46 -0500

    added requirements.txt file

--- script_django/settings.py
+++ script_django/settings.py
@@ -76,6 +76,18 @@ WSGI_APPLICATION = 'script_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'myscriptdb',
+#         'USER': 'masterscript',
+#         'PASSWORD': 'zero0000',
+#         'HOST': 'myscriptdb.cquppea5r7hg.us-east-2.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
