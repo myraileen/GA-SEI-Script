eaf65411edb194e46b8e0d16d53e9345b05928ce
Myra Fah <FahMyra@JohnDeere.com>
2020-04-10 19:18:48 -0500

    updated for heroku deploy

--- script_django/settings.py
+++ script_django/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from dotenv import load_dotenv
+import django_heroku
 
 from pathlib import Path  # python3 only
 env_path = Path('./.env') / '.dotenv'
@@ -156,3 +157,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
